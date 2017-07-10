701f8be87d5a42a8ed91db55b15645e35af01d21
King Claudy <clocloranely@hotmail.fr>
2017-07-10 18:05:32 +0200

    Changement radical du mode de calcul du paramètre de faisceau

--- Simulations/calcul_x.m
+++ Simulations/calcul_x.m
@@ -60,48 +60,45 @@ for idx = 2:numel(z)
         Q2 = Mas*[Q(idx-1);1];
         Q(1,idx) = Q2(1,1)/Q2(2,1); % Retrieving the first line and normalizing
         passed = true;
-        
-    else
-        % Free space matrix (air or silica)
-        B = zTemp - z(idx-1); % Position of the considered point (variable)
-        C = 0;
-        D = 1;
-        Mp = [A,B;
-            C,D];
-        
-        % Computation of the simulated output complex beam parameter
-        Q2 = Mp*[Q(idx-1);1];
-        Q(1,idx) = Q2(1,1)/Q2(2,1); % Retrieving the first line and normalizing
-    end
+            
+        else
+            % Free space matrix (air or silica)
+            B = zTemp - z(idx-1); % Position of the considered point (variable)
+            C = 0;
+            D = 1;
+            Mp = [A,B;
+                C,D];
+            
+            % Computation of the simulated output complex beam parameter
+            Q2 = Mp*[Q(idx-1);1];
+            Q(1,idx) = Q2(1,1)/Q2(2,1); % Retrieving the first line and normalizing
+        end
     
     W(1,idx) = feval(w,zTemp); % Keeping the current waist to find the minimum at the end
-    
-    %     % Computation of the real radius
-    %     R = 1/(1/Q(1,idx)+1i*l*M^2/(pi*Ns*W(1,idx)^2));
-    %     Pr = [Pr,R];
-    
-    U = @(x,zTemp,idx) 1/Q(1,idx)^(M^2/2)... <- For readability
-        .*(exp(-(1i*pi*x.^2)/(l*Q(1,idx)))...
-        .*polyval(hermitePoly(M^2),sqrt(2)*(M.*x)/(W(1,idx)))); % Inline function to compute the amplitude field
-    
-    I = abs(feval(U,x,zTemp,idx).^2);
+       
+%     U = @(x,idx) 1/Q(1,idx)^(M^2/2)... <- For readability
+%         .*(exp(-(1i*pi*x.^2)/(l*Q(1,idx)))...
+%         .*polyval(hermitePoly(M^2),sqrt(2)*(M.*x)/(W(1,idx)))); % Inline function to compute the amplitude field
+    U = @(x,z,idx) sqrt(2/pi)*...
+        ((2*pi/l)*w0)/(2*pi*Q(1,idx))*...
+        exp(1i*(2*pi/l)*(x.^2)/(2*Q(1,idx)));
+    I = 1/2*abs(feval(U,x,z,idx).^2);
     Px(idx,:) = I;
 end
 %% Plotting
 % Intensity
-
 figure; imagesc(Px'); colormap(hot); colorbar;
 ax = gca;
 pIx = find(z >= zi); % Retreving the position of the interface in the matrix
-pFx = find(diff(sign(diff(var(Px,0,2)))) == 2); % Retreving the position of focalisation in the matrix...
-                                                % by calculating the first zero of the first derivative of the variance...
-                                                % AFTER the interface
-
-msgbox(sprintf('The position of focalisation for the curved direction is calculated to be at %gm after the interface.',... <- Displaying the position in a message box
-    ((z(1,end)-z(1,1))*(pFx(2,1)-pIx(1,1))/res)),'Success','Help');
-ax.XTickLabelRotation = 45;
-xlabel('z values'); ax.XTick = [0 pIx(1,1) pFx(2,1) res-1]; ax.XTickLabel = {'0','interface','focalisation','center'};
-ylabel('x values'); ax.YTick = [1 res/2 res]; ax.YTickLabel = {'-k\omega_0','0','k\omega_0'};
+% pFx = find(diff(sign(diff(var(Px,0,2)))) == 2); % Retreving the position of focalisation in the matrix...
+%                                                 % by calculating the first zero of the first derivative of the variance...
+%                                                 % AFTER the interface
+% 
+% msgbox(sprintf('The position of focalisation for the curved direction is calculated to be at %gm after the interface.',... <- Displaying the position in a message box
+%     ((z(1,end)-z(1,1))*(pFx(2,1)-pIx(1,1))/res)),'Success','Help');
+% ax.XTickLabelRotation = 45;
+% xlabel('z values'); ax.XTick = [0 pIx(1,1) pFx(2,1) res-1]; ax.XTickLabel = {'0','interface','focalisation','center'};
+% ylabel('x values'); ax.YTick = [1 res/2 res]; ax.YTickLabel = {'-k\omega_0','0','k\omega_0'};
 
 % Writing images to specific file
 print('PlanXZ','-dpng');
--- Simulations/calcul_y.m
+++ Simulations/calcul_y.m
@@ -61,18 +61,18 @@ for idx = 2:numel(z)
         Q(1,idx) = Q2(1,1)/Q2(2,1); % Retrieving the first line and normalizing
         passed = true;
         
-    else
-        % Free space matrix (air or silica)
-        B = zTemp - z(idx-1); % Position of the considered point (variable)
-        C = 0;
-        D = 1;
-        Mp = [A,B;
-            C,D];
-        
-        % Computation of the simulated output complex beam parameter
-        Q2 = Mp*[Q(idx-1);1];
-        Q(1,idx) = Q2(1,1)/Q2(2,1); % Retrieving the first line and normalizing
-    end
+        else
+            % Free space matrix (air or silica)
+            B = zTemp - z(idx-1); % Position of the considered point (variable)
+            C = 0;
+            D = 1;
+            Mp = [A,B;
+                C,D];
+            
+            % Computation of the simulated output complex beam parameter
+            Q2 = Mp*[Q(idx-1);1];
+            Q(1,idx) = Q2(1,1)/Q2(2,1); % Retrieving the first line and normalizing
+        end
     
     W(1,idx) = feval(w,zTemp); % Keeping the current waist to find the minimum at the end
     
@@ -84,7 +84,7 @@ for idx = 2:numel(z)
         .*(exp(-(1i*pi*x.^2)/(l*Q(1,idx)))...
         .*polyval(hermitePoly(M^2),sqrt(2)*(M.*x)/(W(1,idx)))); % Inline function to compute the amplitude field
     
-    I = abs(feval(U,y,zTemp,idx).^2);
+    I = 1/2*abs(feval(U,y,zTemp,idx).^2);
     Py(idx,:) = I;
 end
 %% Plotting
