520382bf3139f9f723d84d197dfe55bd83e2f549
King Claudy <clocloranely@hotmail.fr>
2017-07-14 09:45:43 +0200

    Changement du mode de calculs des ellipses

--- Simulations/calcul_ellipse.m
+++ Simulations/calcul_ellipse.m
@@ -1,16 +1,28 @@
 function [ dX,dY ] = calcul_ellipse( Px,Py,pt,thr)
 %calcul_ellipse Calculate the semi-minor axis and the semi-major axis ellipse at the given point
-%   pt is the double at which the  computation takes place
+%   pt is the double at which the computation takes place
 
 Lx = find(Px(pt,:) >= thr); % Searching for the value corresponding to the beam diameter
-Lx1 = Lx(1,1); % Lower bound for the beam diameter
-Lx2 = Lx(1,end); % Upper bound for the beam diameter
-dX = Lx2 - Lx1; % Beam diameter in pixel
+if isempty(Lx) % Taking care of the empty case by returning zeros axis
+    dX = 0;
+    dY = 0;
+    return;
+else
+    Lx1 = Lx(1,1); % Lower bound for the beam diameter
+    Lx2 = Lx(1,end); % Upper bound for the beam diameter
+    dX = Lx2 - Lx1; % Beam diameter in pixel
+end
 
 Ly = find(Py(pt,:) >= thr); % Searching for the value corresponding to the beam diameter
-Ly1 = Ly(1,1); % Lower bound for the beam diameter
-Ly2 = Ly(1,end); % Upper bound for the beam diameter
-dY = Ly2 - Ly1; % Beam diameter in pixel
+if isempty(Ly) % Taking care of the empty case by returning zeros axis
+    dX = 0;
+    dY = 0;
+    return;
+else
+    Ly1 = Ly(1,1); % Lower bound for the beam diameter
+    Ly2 = Ly(1,end); % Upper bound for the beam diameter
+    dY = Ly2 - Ly1; % Beam diameter in pixel
+end
 
 end
 
